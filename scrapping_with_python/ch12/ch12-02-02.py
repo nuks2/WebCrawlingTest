39394d1201dd096f764f90026e085fb788f4dc84
Yi Jinuk <jinuk.yi@twolinecode.com>
2021-10-27 12:30:46 +0900

    12장 추가

---
+++ scrapping_with_python/ch12/ch12-02-02.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+
+driver = webdriver.PhantomJS(executable_path='./phantomjs-2.1.1-macosx/bin/phantomjs')
+driver.get("http://pythonscraping.com")
+driver.implicitly_wait(1)
+print(driver.get_cookies())
+print('-----------------------------------------------------------------------------')
+
+savedCookies = driver.get_cookies()
+
+driver2 = webdriver.PhantomJS(executable_path='./phantomjs-2.1.1-macosx/bin/phantomjs')
+# driver2 = webdriver.Chrome(executable_path='/Volumes/DevHD/LearningProjects/WebCrawlingTest/web_driver/chromedriver')   
+driver2.get("http://pythonscraping.com")
+driver2.implicitly_wait(1)
+driver2.delete_all_cookies()
+for cookie in savedCookies:
+    print('cookie: ', cookie)
+    driver2.add_cookie(cookie)
+
+
+driver2.get("http://pythonscraping.com")
+driver.implicitly_wait(1)
+print(driver2.get_cookies())
