065a87039fb0341db682d417cfd2cf1d822239c2
Yi Jinuk <jinuk.yi@twolinecode.com>
2021-10-28 18:14:13 +0900

    정보추가

--- scrapping_with_python/ch12/ch12-02-01.py
+++ scrapping_with_python/ch12/ch12-02-01.py
@@ -1,6 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 
+
+# Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1 Safari/605.1.15
+# Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36
+
 session = requests.Session()
 headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit 537.36 (KHTML, like Gecko) Chrome"
         ,"Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"}
